8fab4de0bbcf3e6cbd791570ba0e52ef0be50394
Dmitry Andreev <da@shiftam.com>
2022-08-04 12:55:56 +0300

    fixed bug - wetch implementation abi, if proxy

--- src/web3_premium/utils.py
+++ src/web3_premium/utils.py
@@ -1,5 +1,15 @@
+import json
+
 from .explorer import Explorer
 
 
 def fetch_abi(explorer: Explorer, address: str):
-    return explorer.contract.getabi(address=address)
+    contract_info = explorer.contract.getsourcecode(address=address)[0]
+    abi = json.loads(contract_info["ABI"])
+    if contract_info["Proxy"] == "1":
+        implementation_info = explorer.contract.getsourcecode(
+            address=contract_info["Implementation"]
+        )[0]
+        abi = json.loads(implementation_info["ABI"])
+
+    return abi
