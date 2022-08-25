6a3cfa006532f0e30cc041b848cfd391ce0705c8
Dmitry Andreev <da@shiftam.com>
2022-08-25 14:17:40 +0300

    added native tokens, changed avalanche rpc

--- src/web3_premium/chains.py
+++ src/web3_premium/chains.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Optional
 
 from web3 import Web3
@@ -14,20 +15,28 @@ from .explorer import (
 )
 
 
+@dataclass
+class NativeToken:
+    symbol: str
+    decimals: int = 18
+
+
 class Chain(Web3):
     explorer: Optional[Explorer]
+    native_token: Optional[NativeToken]
 
 
-def chain_w3(rpc, explorer=None) -> Chain:
+def chain_w3(rpc, explorer=None, symbol: str = None, decimals=18) -> Chain:
     w3 = Web3(Web3.HTTPProvider(rpc))
     w3.explorer = explorer
+    w3.native_token = NativeToken(symbol, decimals) if symbol else None
     return w3
 
 
-ethereum = chain_w3("https://rpc.ankr.com/eth", etherscan)
-avalanche = chain_w3("https://rpc.ankr.com/avalanche-c", snowtrace)
-fantom = chain_w3("https://rpc.ankr.com/fantom", ftmscan)
-arbitrum = chain_w3("https://rpc.ankr.com/arbitrum", arbiscan)
-bsc = chain_w3("https://rpc.ankr.com/bsc", bscscan)
-optimism = chain_w3("https://rpc.ankr.com/optimism", optimistic_etherscan)
-polygon = chain_w3("https://rpc.ankr.com/polygon", polygonscan)
+ethereum = chain_w3("https://rpc.ankr.com/eth", etherscan, "ETH")
+avalanche = chain_w3("https://rpc.ankr.com/avalanche", snowtrace, "AVAX")
+fantom = chain_w3("https://rpc.ankr.com/fantom", ftmscan, "FTM")
+arbitrum = chain_w3("https://rpc.ankr.com/arbitrum", arbiscan, "ETH")
+bsc = chain_w3("https://rpc.ankr.com/bsc", bscscan, "BNB")
+optimism = chain_w3("https://rpc.ankr.com/optimism", optimistic_etherscan, "ETH")
+polygon = chain_w3("https://rpc.ankr.com/polygon", polygonscan, "MATIC")
